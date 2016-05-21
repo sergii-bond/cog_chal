c20c651b8213bcebf7126bd01ca1fdd2470e42cb
sergii-bondariev <SergeyBondarev@gmail.com>
2016-05-21 11:15:12 -0700

    code to download flickr images with the given user name and tag, csv file is created to keep a map between image paths on disk and urls

---
+++ get_flickr.py
@@ -0,0 +1,41 @@
+import flickrapi
+import xml.etree.ElementTree as ET
+import urllib
+import os
+#import hdf5
+import csv
+
+def dic2csv(dic, csv_path, field1, field2):
+
+  with open(csv_path, 'w') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames = [field1, field2])
+    writer.writeheader()
+
+    for key, val in dic.iteritems():
+        writer.writerow({field1 : key,
+                         field2 : val
+                        })
+
+
+flickr_photos_dir = 'flickr_photos'
+api_key = u'c871b759c44a9c7d3ced100ba2cf4dd0'
+api_secret = u'da8022494490182e'
+
+name_to_url = {}
+
+flickr = flickrapi.FlickrAPI(api_key, api_secret)
+for photo in flickr.walk(user_id='143060054@N02', tags = 'people'):
+    #print ET.tostring(photo, encoding='utf8', method='xml')
+    #url = 'http://farm' + photo.get('farm') + '.staticflickr.com/' + \
+    #photo.get('server') + '/' + photo.get('id') + '_' + photo.get('secret') + '_n.jpg'
+    imname = photo.get('id') + '_' + photo.get('secret') + '.jpg'
+    url = 'http://static.flickr.com/' + photo.get('server') + '/' + imname
+
+    print 'Fetching ' + url
+    impath = os.path.join(flickr_photos_dir, imname) 
+    urllib.urlretrieve(url, impath)
+    name_to_url[impath] = url 
+
+
+
+dic2csv(name_to_url, 'im2url.csv', 'impath', 'url')
